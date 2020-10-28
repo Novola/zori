8f3dd2937c9e5bee2af78ac33647b0cddff469c9
CHARDNOL99 <62255498+CHARDNOL99@users.noreply.github.com>
2020-10-28 12:45:51 +0000

    Update and rename Reach.java to extendReach.java

--- src/main/java/club/novola/zori/module/combat/extendReach.java
+++ src/main/java/club/novola/zori/module/combat/extendReach.java
@@ -3,9 +3,9 @@ package club.novola.zori.module.combat;
 import club.novola.zori.setting.Setting;
 import club.novola.zori.module.Module;
 
-public class Reach extends Module {
-    public Reach() {
-        super("Reach", Category.COMBAT);
+public class extendReach extends Module {
+    public extendReach() {
+        super("extendReach", Category.COMBAT);
     }
 
     public Setting<Float> distance = register("Distance", 5.5f, 0.0f, 10.0f); // make sure the compiler doesnt think the values are doubles
