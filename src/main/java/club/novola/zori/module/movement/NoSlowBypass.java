1be2e86a00364662c6d0a5d7574d13a09b49513b
novola <61128456+Novola@users.noreply.github.com>
2020-10-30 22:12:24 +0000

    Add files via upload

---
+++ src/main/java/club/novola/zori/module/movement/NoSlowBypass.java
@@ -0,0 +1,38 @@
+package club.novola.zori.module.movement;
+
+import club.novola.zori.setting.Setting;
+import club.novola.zori.module.Module;
+import club.novola.zori.util.Wrapper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemPotion;
+import net.minecraft.network.play.client.CPacketEntityAction;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class NoSlowBypass extends Module {
+
+    public NoSlowBypass() {
+        super("NoSlowBypass", Category.MOVEMENT);
+    }
+
+    private boolean sneaking;
+
+    @Override
+    public void onUpdate() {
+        Item item = Wrapper.getPlayer().getActiveItemStack().getItem();
+        if (sneaking && ((!Wrapper.getPlayer().isHandActive() && item instanceof ItemFood || item instanceof ItemBow || item instanceof ItemPotion) || (!(item instanceof ItemFood) || !(item instanceof ItemBow) || !(item instanceof ItemPotion)))) {
+            Wrapper.getPlayer().connection.sendPacket(new CPacketEntityAction(Wrapper.getPlayer(), CPacketEntityAction.Action.STOP_SNEAKING));
+            sneaking = false;
+        }
+    }
+
+    @SubscribeEvent
+    public void onUseItem(LivingEntityUseItemEvent event) {
+        if (!sneaking) {
+            Wrapper.getPlayer().connection.sendPacket(new CPacketEntityAction(Wrapper.getPlayer(), CPacketEntityAction.Action.START_SNEAKING));
+            sneaking = true;
+        }
+    }
+}
