94dcc07c295e7c5515cfec9da2fd6e599e50d6e7
novola <61128456+Novola@users.noreply.github.com>
2020-11-06 19:48:43 +0000

    Added ToggleMsg.java

---
+++ src/main/java/club/novola/zori/module/misc/ToggleMsg.java
@@ -0,0 +1,10 @@
+package club.novola.zori.module.misc;
+
+import club.novola.zori.module.Module;
+import club.novola.zori.util.Wrapper;
+
+public class ToggleMsg extends Module {
+    public ToggleMsg() {
+        super("ToggleMsg", Category.MISC);
+    }
+}
